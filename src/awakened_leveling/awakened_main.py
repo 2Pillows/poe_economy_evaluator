f4230df1964c6f18542578f688a3d1ca81edad60
Pillows <25855654+2Pillows@users.noreply.github.com>
2024-01-12 22:20:17 -0500

    added awakened leveling

---
+++ src/awakened_leveling/awakened_main.py
@@ -0,0 +1,100 @@
+# main.py
+
+import os
+import requests
+
+LEAUGE_NAME = "Affliction"
+MIN_PROFIT = 10
+
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.dirname(CUR_DIR))
+RESULTS_FILE = os.path.join(PROJECT_DIR, "results", "awakened_leveling.txt")
+
+GEM_URL = f"https://poe.ninja/api/data/itemoverview?league={LEAUGE_NAME}&type=SkillGem"
+BEAST_URL = f"https://poe.ninja/api/data/itemoverview?league={LEAUGE_NAME}&type=Beast"
+CURRENCY_URL = (
+    f"https://poe.ninja/api/data/currencyoverview?league={LEAUGE_NAME}&type=Currency"
+)
+
+
+def fetch_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while fetching data: {e}")
+
+    return data
+
+
+def sort_gem_data(gem_data):
+    sorted_gems = {}
+
+    for gem in gem_data:
+        gem_name = gem.get("name")
+        if "Awakened" not in gem_name:
+            continue
+        if "Empower" in gem_name or "Enhance" in gem_name or "Enlighten" in gem_name:
+            continue
+        gem_level = gem.get("gemLevel")
+        gem_chaos = gem.get("chaosValue")
+        if gem_name not in sorted_gems:
+            sorted_gems[gem_name] = {}
+        sorted_gems[gem_name][gem_level] = gem_chaos
+
+    return sorted_gems
+
+
+def write_to_file(gem_data):
+    with open(RESULTS_FILE, "w") as file:
+        for name, profit in gem_data.items():
+            if profit[0] <= MIN_PROFIT:
+                continue
+            formatted_line = f"{name:50} | {round(profit[0]):10} {round(profit[1]), round(profit[2])}"
+            file.write(formatted_line + "\n")
+
+
+def start_awakened_main():
+    gem_data = fetch_data(GEM_URL)["lines"]
+
+    gem_levels = sort_gem_data(gem_data)
+
+    beast_data = fetch_data(BEAST_URL)["lines"]
+
+    wild_brambleback = {
+        beast.get("name"): beast
+        for beast in beast_data
+        if "Wild Brambleback" in beast.get("name")
+    }["Wild Brambleback"]
+    wild_brambleback_chaos = wild_brambleback.get("chaosValue")
+
+    currency_data = fetch_data(CURRENCY_URL)["lines"]
+
+    gemcutter_chaos = {
+        currency_item.get("currencyTypeName"): currency_item.get("chaosEquivalent")
+        for currency_item in currency_data
+        if "Gemcutter's Prism" in currency_item.get("currencyTypeName")
+    }["Gemcutter's Prism"]
+
+    gem_margins = {
+        name: [
+            prices[5]
+            - prices[1]
+            - (4 * wild_brambleback_chaos)
+            - (20 * gemcutter_chaos),
+            prices[1],
+            prices[5],
+        ]
+        for name, prices in gem_levels.items()
+        if 5 in prices and 1 in prices
+    }
+
+    sorted_gem_margins = dict(
+        sorted(gem_margins.items(), key=lambda item: item[1], reverse=True)
+    )
+
+    write_to_file(sorted_gem_margins)
+    # os.system(f"start {FILE_NAME}")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from harvest_rolling.harvest_main import start_harvest_main
 from sanctum_rewards.sanctum_main import start_sanctum_main
 from sextant_rolling.sextant_main import start_sextant_main
+from awakened_leveling.awakened_main import start_awakened_main
 
 
 def main():
     start_harvest_main()
     start_sanctum_main()
     start_sextant_main()
+    start_awakened_main()
 
 
 if __name__ == "__main__":
