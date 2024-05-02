362fd3d5cc8b6923ad8f6763de9b2d3933f0ca3e
2Pillows <25855654+2Pillows@users.noreply.github.com>
2024-05-02 23:52:23 +0000

    finished leveling page

--- frontend/src/pages/awakened-leveling.jsx
+++ frontend/src/pages/awakened-leveling.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./awakened-leveling.css";
 
 const Awakened_Leveling = ({ apiData }) => {
-  console.log(apiData);
+  // console.log(apiData);
   const [divine, setDivine] = useState(apiData ? apiData.divine : 0);
   const [gem_data, setGemData] = useState(apiData ? apiData.gem_data : []);
   const [gem_margins, setGemMargins] = useState(
@@ -38,29 +38,58 @@ const Awakened_Leveling = ({ apiData }) => {
   //   fetchData();
   // }, []);
 
-  // Function to handle input change and update divine state
-  const handleInputChange = (event) => {
-    const value = parseFloat(event.target.value); // Parsing input value to integer
-    setGemcutter(isNaN(value) ? 0 : value); // Set to 0 if NaN
-    createPost(value); // Trigger createPost function on input change
-  };
+  const gemValueChange = (event, gemName, priceType, currencyType) => {
+    console.log("Gem Value Change - Event:", event.target.value);
+    console.log("Gem Value Change - Gem Name:", gemName);
+    console.log("Gem Value Change - Price Type:", priceType);
+    console.log("Gem Value Change - Currency Type:", currencyType);
 
-  // Function to create a new post
-  const createPost = async (divineValue) => {
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/api/awakened_leveling",
-        {
-          gemcutter: divineValue, // Send the updated divine value
-        }
-      );
-      setGemMargins(response.data.gem_margins);
-    } catch (error) {
-      console.error("Error creating post:", error);
+    // Copy the current gem margins to avoid mutating state directly
+    const updatedGemMargins = { ...gem_margins };
+
+    let newValue = parseFloat(event.target.value); // udpated input value
+    if (currencyType == "divine") {
+      newValue *= divine;
     }
+
+    let oldValue = updatedGemMargins[gemName]["old" + priceType]
+      ? updatedGemMargins[gemName]["old" + priceType]
+      : newValue;
+    delete updatedGemMargins[gemName]["old" + priceType];
+
+    console.log("Gem Value Change - Event:", newValue);
+    console.log("Gem Value Change - Old Price:", oldValue);
+    updatedGemMargins[gemName][priceType] = newValue;
+
+    // make ngative if buying price
+    if (priceType == "buy") {
+      newValue *= -1;
+      oldValue *= -1;
+    }
+
+    updatedGemMargins[gemName]["profit"] -= oldValue;
+    updatedGemMargins[gemName]["profit"] += newValue;
+
+    setGemMargins(updatedGemMargins);
   };
 
-  const gemValueChange = (event) => {};
+  const handleGemInputChange = (event, gemName, priceType, currencyType) => {
+    const updatedGemMargins = { ...gem_margins };
+
+    let value = parseFloat(event.target.value); // Parsing input value to integer
+    if (currencyType == "divine") {
+      value *= divine;
+    }
+    let oldValue = updatedGemMargins[gemName][priceType];
+
+    updatedGemMargins[gemName][priceType] = value;
+
+    if (!updatedGemMargins[gemName]["old" + priceType]) {
+      updatedGemMargins[gemName]["old" + priceType] = oldValue;
+    }
+
+    setGemMargins(updatedGemMargins);
+  };
 
   const displayGemMargins = (gem_margins) => {
     // Convert gem_margins object to an array of [key, value] pairs
@@ -97,15 +126,21 @@ const Awakened_Leveling = ({ apiData }) => {
             <input
               className="gem-chaos"
               type="number"
-              value={value.buy.toFixed(0)}
-              onChange={gemValueChange}
+              value={parseFloat(value.buy.toFixed(0))}
+              onChange={(event) =>
+                handleGemInputChange(event, key, "buy", "chaos")
+              }
+              onBlur={(event) => gemValueChange(event, key, "buy", "chaos")}
             ></input>
             <span className="price-separator">|</span>
             <input
               className="gem-divine"
               type="number"
-              value={(value.buy / divine).toFixed(1)}
-              onChange={gemValueChange}
+              value={parseFloat((value.buy / divine).toFixed(1))}
+              onChange={(event) =>
+                handleGemInputChange(event, key, "buy", "divine")
+              }
+              onBlur={(event) => gemValueChange(event, key, "buy", "divine")}
             ></input>
           </td>
 
@@ -113,15 +148,21 @@ const Awakened_Leveling = ({ apiData }) => {
             <input
               className="gem-chaos"
               type="number"
-              value={value.sell.toFixed(0)}
-              onChange={gemValueChange}
+              value={parseFloat(value.sell.toFixed(0))}
+              onChange={(event) =>
+                handleGemInputChange(event, key, "sell", "chaos")
+              }
+              onBlur={(event) => gemValueChange(event, key, "sell", "chaos")}
             ></input>
             <span className="price-separator">|</span>
             <input
               className="gem-divine"
               type="number"
-              value={(value.sell / divine).toFixed(1)}
-              onChange={gemValueChange}
+              value={parseFloat((value.sell / divine).toFixed(1))}
+              onChange={(event) =>
+                handleGemInputChange(event, key, "sell", "divine")
+              }
+              onBlur={(event) => gemValueChange(event, key, "sell", "divine")}
             ></input>
           </td>
         </tr>
@@ -159,17 +200,84 @@ const Awakened_Leveling = ({ apiData }) => {
     );
   };
 
+  // Function to handle input change and update divine state
+  const handleOptionInputChange = (event, setter, key) => {
+    const value = parseFloat(event.target.value);
+    setter(isNaN(value) ? 0 : value);
+    createPost(key, value);
+  };
+
+  // Function to create a new post
+  const createPost = async (key, value) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/awakened_leveling",
+        {
+          [key]: value,
+        }
+      );
+      setGemMargins(response.data.gem_margins);
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+  };
+
+  // Define your options as an array of objects
+  const options = [
+    {
+      label: "Divine: ",
+      key: "divine",
+      value: divine,
+      setter: setDivine,
+      onChange: handleOptionInputChange,
+    },
+    {
+      label: "Gemcutter: ",
+      key: "gemcutter",
+      value: gemcutter,
+      setter: setGemcutter,
+      onChange: handleOptionInputChange,
+    },
+    {
+      label: "Wild Brambeback: ",
+      key: "wild_brambleback",
+      value: wild_brambleback,
+      setter: setWildBrambleback,
+
+      onChange: handleOptionInputChange,
+    },
+
+    // Add more options as needed
+  ];
+
+  const displayOptions = () => {
+    return (
+      <div className="options-grid">
+        {options.map((option, index) => (
+          <div className="options" key={index}>
+            <label>{option.label}</label>
+            <input
+              className="options-input"
+              type="number"
+              value={option.value}
+              onChange={(event) =>
+                option.onChange(event, option.setter, option.key)
+              }
+            />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  // Render the options grid dynamically
   return (
     <div className="content">
       <div className="heading">
         <h1 className="content-title">Awakened Leveling</h1>
+        {displayOptions()}
       </div>
-      <div className="info">
-        {/* Text field to display and edit the divine value */}
-        <label>Divine:</label>
-        <input type="number" value={gemcutter} onChange={handleInputChange} />
-        {displayGemMargins(gem_margins)}
-      </div>
+      <div className="info">{displayGemMargins(gem_margins)}</div>
     </div>
   );
 };
