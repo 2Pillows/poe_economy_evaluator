c09e51930f206e4cded0298ad487a8a13825a8b9
2Pillows <25855654+2Pillows@users.noreply.github.com>
2024-07-16 18:17:37 +0000

    pass chaos instead of dict

--- backend/scripts/awakened_leveling/awakened_main.py
+++ backend/scripts/awakened_leveling/awakened_main.py
@@ -1,13 +1,13 @@
 # awakened_main.py
 
-from backend.scripts.api_data import API_Data
+from backend.scripts.api_data import API_Data, ScriptData
 from backend.scripts.keys import Keys
 
 MIN_PROFIT = 10
 RESULTS_FILE = "/workspaces/poe_economy_evaluator/backend/results/awakened_leveling.txt"
 
 
-class AwakenedLevelingData:
+class AwakenedLevelingData(ScriptData):
     def __init__(self, keys: Keys):
         api_data = API_Data().all_data
 
@@ -18,29 +18,24 @@ class AwakenedLevelingData:
         self.max_lvl_gems = api_data[keys.MAX_LVL_AWAKENED_GEMS]
         self.max_lvl_max_qual_gems = api_data[keys.MAX_LVL_QUAL_AWAKENED_GEMS]
 
+        self.level_cost = 4 * self.wild_brambleback
+        self.quality_cost = 20 * self.gemcutter
+
         self.gem_margins = None
-        self.level_cost = 4 * self.wild_brambleback[keys.CHAOS]
-        self.quality_cost = 20 * self.gemcutter[keys.CHAOS]
-
-    def __getattr__(self, attr):
-        return API_Data().all_data[attr]
-
-    def __setattr__(self, attr, value):
-        API_Data().all_data[attr] = value
 
 
 def start_awakened_main():
     keys = Keys()
     leveling_data = AwakenedLevelingData(keys)
 
-    gem_margins = calculate_gem_margins(leveling_data, keys)
+    gem_margins = calculate_gem_margins(leveling_data)
 
     leveling_data.gem_margins = gem_margins
 
-    write_to_file(leveling_data, keys)
+    write_to_file(leveling_data)
 
 
-def calculate_gem_margins(leveling_data: AwakenedLevelingData, keys: Keys):
+def calculate_gem_margins(leveling_data: AwakenedLevelingData):
     gem_margins = {}
 
     for name, min_gem in leveling_data.min_lvl_gems.items():
@@ -52,12 +47,12 @@ def calculate_gem_margins(leveling_data: AwakenedLevelingData, keys: Keys):
         # Max level wo/ quality. Need to add quality cost to chaos
         elif name in leveling_data.max_lvl_gems:
             max_gem = leveling_data.max_lvl_gems[name]
-            max_gem[keys.CHAOS] += leveling_data.quality_cost
+            max_gem += leveling_data.quality_cost
         else:
             continue
 
-        min_price = min_gem[keys.CHAOS]
-        max_price = max_gem[keys.CHAOS]
+        min_price = min_gem
+        max_price = max_gem
 
         profit = (
             max_price
@@ -74,14 +69,14 @@ def calculate_gem_margins(leveling_data: AwakenedLevelingData, keys: Keys):
     return sorted_gem_margins
 
 
-def write_to_file(leveling_data: AwakenedLevelingData, keys: Keys):
+def write_to_file(leveling_data: AwakenedLevelingData):
     with open(RESULTS_FILE, "w") as file:
         file.write(f"{'Gem Name':50} | {'':7} Profit {'':10} Buy {'':12} Sell\n")
         for name, margin in leveling_data.gem_margins.items():
             profit = margin["profit"]
             buy = margin["buy"]
             sell = margin["sell"]
-            divine_price = leveling_data.divine[keys.CHAOS]
+            divine_price = leveling_data.divine
             if profit <= MIN_PROFIT:
                 continue
             formatted_line = f"{name:50} | \t {round(profit):>5} | {round(profit/divine_price, 1):>5}  {round(buy):>8} | {round(buy/divine_price, 1):>5}  {round(sell):>8} | {round(sell/divine_price, 1):>5}"
