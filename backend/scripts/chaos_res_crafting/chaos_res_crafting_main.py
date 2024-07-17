a3f2f0f3fbf2678077afc5a0a019ae68d3a35d0e
2Pillows <25855654+2Pillows@users.noreply.github.com>
2024-07-17 21:00:22 +0000

    update odds for harvest and essence

--- backend/scripts/chaos_res_crafting/chaos_res_crafting_main.py
+++ backend/scripts/chaos_res_crafting/chaos_res_crafting_main.py
@@ -5,14 +5,17 @@ from backend.scripts.keys import Keys
 
 RESULTS_FILE = "backend/results/chaos_res_crafting.txt"
 
-# Harvest crafting for T1 Chaos Res is 1/7
-# Each reforge chaos is 70 yellow lifeforce
-yellow_needed = 700
 
 # average rolls
+# harvest is 1/31 for same rolls,
+AVG_HARVEST_RES_LIFE = 31
 
-# deafening envy and getting t3+ res w/ life or open prefix
-AVG_ENVY_RES_LIFE = 6
+# Harvest crafting for T1 Chaos Res is 1/7
+# Each reforge chaos is 100 yellow lifeforce
+YELLOW_NEEDED = 7 * 100 * AVG_HARVEST_RES_LIFE
+
+# deafening envy and getting t3+ res and (t3+ life or open prefix)
+ENVY_NEEDED = 6
 
 
 class ChaosResCraftingData:
@@ -30,7 +33,8 @@ class ChaosResCraftingData:
         self.scour_cost = api_data[keys.SCOUR][keys.CHAOS]
         self.alch_cost = api_data[keys.ALCH][keys.CHAOS]
 
-        self.harvest_cost = None
+        self.harvest_crafting_cost = None
+        self.deafening_crafting_cost = None
         self.yellow_equiv = None
         self.envy_equiv = None
         self.stygian_crafting_cost = None
@@ -40,21 +44,23 @@ def start_chaos_res_crafting():
     keys = Keys()
     crafting_data = ChaosResCraftingData(keys)
 
-    harvest_cost = yellow_needed / crafting_data.yellow_lifeforce_per_chaos
+    harvest_crafting_cost = YELLOW_NEEDED / crafting_data.yellow_lifeforce_per_chaos
+    deafening_crafting_cost = crafting_data.deafening_envy_cost * ENVY_NEEDED
 
-    yellow_equiv = yellow_needed / crafting_data.deafening_envy_cost
-    envy_equiv = crafting_data.deafening_envy_cost / yellow_needed
+    yellow_equiv = YELLOW_NEEDED / deafening_crafting_cost
+    envy_equiv = deafening_crafting_cost / YELLOW_NEEDED
 
     # find cost of stygian crafting
     stygian_cost = (
         crafting_data.stygian_base_cost
-        + (min(harvest_cost, crafting_data.deafening_envy_cost) * AVG_ENVY_RES_LIFE)
+        + min(harvest_crafting_cost, deafening_crafting_cost)
         + (4 * crafting_data.fertile_catalyst_cost)
         + crafting_data.scour_cost
         + crafting_data.alch_cost
     )
 
-    crafting_data.harvest_cost = harvest_cost
+    crafting_data.harvest_crafting_cost = harvest_crafting_cost
+    crafting_data.deafening_crafting_cost = deafening_crafting_cost
     crafting_data.yellow_equiv = yellow_equiv
     crafting_data.envy_equiv = envy_equiv
     crafting_data.stygian_crafting_cost = stygian_cost
@@ -70,19 +76,16 @@ def write_results(crafting_data: ChaosResCraftingData):
     # write data to file
     with open(RESULTS_FILE, "a", encoding="utf-8") as file:
         file.write("\n---------- Rolling Cost ----------\n\n")
-        yellow_lifeforce_per_chaos = 1 / crafting_data.yellow_lifeforce_per_chaos
-        yellow_lifeforce_per_div = (
-            yellow_lifeforce_per_chaos * crafting_data.divine_cost
-        )
+
         file.write(
-            f"Harvest Reforge Chaos (~700 Yellow): {round(crafting_data.harvest_cost)} chaos total \n\tBuy: {round(yellow_lifeforce_per_chaos)} per chaos | {round(yellow_lifeforce_per_div):,} per div\n"
+            f"Harvest Reforge Chaos (~{YELLOW_NEEDED} Yellow): {round(crafting_data.harvest_crafting_cost)} chaos total \n\tBuy: {round(crafting_data.yellow_lifeforce_per_chaos)} per chaos | {round(crafting_data.yellow_lifeforce_per_chaos * crafting_data.divine_cost):,} per div\n"
         )
         if crafting_data.yellow_equiv >= 1:
             file.write(
                 f"\tEquiv to Essence: {round(crafting_data.yellow_equiv)} per chaos | {round(crafting_data.yellow_equiv * crafting_data.divine_cost):,} per div\n\n"
             )
         file.write(
-            f"Deafening Envy: {round(crafting_data.deafening_envy_cost)} chaos | {round(crafting_data.divine_cost / crafting_data.deafening_envy_cost, 2)} per div\n"
+            f"Essence Spamming (~{ENVY_NEEDED} Envy): {round(crafting_data.deafening_crafting_cost)} chaos total \n\tBuy: {round(crafting_data.deafening_envy_cost)} chaos | {round(crafting_data.divine_cost/crafting_data.deafening_envy_cost):,} per div\n"
         )
         if crafting_data.envy_equiv >= 1:
             file.write(
@@ -93,13 +96,13 @@ def write_results(crafting_data: ChaosResCraftingData):
         file.write(
             f"Stygian Vise: {round(crafting_data.stygian_crafting_cost)}c avg total \n\tiLvl 86 Base: {round(crafting_data.stygian_base_cost)}c \n\t4x Fertile Catalyst: {round(4 * crafting_data.fertile_catalyst_cost)} chaos total | {round(crafting_data.fertile_catalyst_cost)} chaos per or {round(crafting_data.divine_cost / crafting_data.fertile_catalyst_cost, 2)} per div \n"
         )
-        if crafting_data.harvest_cost < crafting_data.deafening_envy_cost:
+        if crafting_data.harvest_crafting_cost < crafting_data.deafening_crafting_cost:
             file.write(
-                f"\t {round(AVG_ENVY_RES_LIFE * yellow_needed)}x Yellow Juice: {round(crafting_data.harvest_cost * AVG_ENVY_RES_LIFE)} chaos total"
+                f"\t {round(YELLOW_NEEDED)}x Yellow Juice: {round(crafting_data.harvest_crafting_cost)} chaos total"
             )
         else:
             file.write(
-                f"\t{AVG_ENVY_RES_LIFE}x Deafening Envy: {round(crafting_data.deafening_envy_cost * AVG_ENVY_RES_LIFE)} chaos total"
+                f"\t{ENVY_NEEDED}x Deafening Envy: {round(crafting_data.deafening_crafting_cost)} chaos total"
             )
 
 
